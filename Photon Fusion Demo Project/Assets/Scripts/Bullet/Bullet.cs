9512c5ffdbf73bf79608240910e7a907b65395bf
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2023-03-08 09:46:57 +0700

    RPC & Health

--- Photon Fusion Demo Project/Assets/Scripts/Bullet/Bullet.cs
+++ Photon Fusion Demo Project/Assets/Scripts/Bullet/Bullet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,10 +9,13 @@ public class Bullet : NetworkBehaviour
     [SerializeField] private float bulletSpeed = 5f;
     [Networked] private TickTimer life { get; set; }
 
+   
+
     public override void Spawned()
     {
         life = TickTimer.CreateFromSeconds(Runner, 5f);
     }
+    
 
     public override void FixedUpdateNetwork()
     {
@@ -25,4 +29,16 @@ public class Bullet : NetworkBehaviour
             transform.position += bulletSpeed * transform.forward * Runner.DeltaTime;
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            var player = other.GetComponent<PlayerController>();
+            player.TakeDamage(30);
+            Runner.Despawn(Object);
+        }
+    }
+
+  
 }
